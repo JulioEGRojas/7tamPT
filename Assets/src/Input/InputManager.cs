4c1bf006936c948bf65b0a32471ba5ad41fc2864
JulioRojas <Gitito3347._>
2024-07-13 14:02:55 -0600

    Adds player controller for testing movement.

--- Assets/src/Input/InputManager.cs
+++ Assets/src/Input/InputManager.cs
@@ -1,19 +1,23 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class InputManager : MonoBehaviour {
 
     [Header("Movement Input")]
+    [SerializeField] private bl_Joystick moveJoystick;
+    
     [SerializeField] private InputActionReference movementAction;
 
-    [SerializeField] private Vector2ScriptableValue movementInputValue;
+    [SerializeField] private Vector2ScriptableValue moveVector;
 
     private bool _immunityOnCooldown;
 
     private void Start() {
         // This is needed because if we loose while moving and restart, value starts as not zero. 
-        movementInputValue.SetValue(Vector2.zero);
+        moveVector.SetValue(Vector2.zero);
     }
 
     private void OnEnable() {
@@ -26,12 +30,16 @@ public class InputManager : MonoBehaviour {
         movementAction.action.performed -= OnMovementActionPerformed;
     }
 
+    private void Update() {
+        moveVector.SetValue(new Vector2(moveJoystick.Horizontal,moveJoystick.Vertical));
+    }
+
     /// <summary>
     /// Movement action only sets the value of the movement vector. Player script then uses it to move.
     /// </summary>
     /// <param name="obj"></param>
     private void OnMovementActionPerformed(InputAction.CallbackContext obj) {
         Vector2 axisMovementInput = obj.ReadValue<Vector2>();
-        movementInputValue.SetValue(axisMovementInput);
+        moveVector.SetValue(axisMovementInput);
     }
 }
