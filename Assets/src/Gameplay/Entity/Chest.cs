b6a25538d83cccde39df720671ae0b5b21407050
JulioRojas <Gitito3347._>
2024-07-14 21:12:59 -0600

    Scatter items when chest opened. Coins now render after.

--- Assets/src/Gameplay/Entity/Chest.cs
+++ Assets/src/Gameplay/Entity/Chest.cs
@@ -8,6 +8,9 @@ public class Chest : Shootable {
 
     private bool _looted = false;
 
+    /// <summary>
+    /// Instantiates all the loot on the lootWhenShot list
+    /// </summary>
     public void InstantiateLoot() {
         if (_looted) {
             return;
@@ -17,6 +20,8 @@ public class Chest : Shootable {
         foreach (GameObject lootPrefab in lootWhenShot) {
             GameObject instance = Instantiate(lootPrefab.gameObject, transform.position, Quaternion.identity);
             instance.SetActive(true);
+            // Moves a bit the items so they scatter around
+            instance.transform.Translate(new Vector3(Random.Range(0,0.25f), Random.Range(0,0.25f), 0));
         }
     }
 }
